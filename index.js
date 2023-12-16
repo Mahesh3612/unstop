de942a56481969260e4c0183beab97d4fc6fea26
Mahesh3612 <maheshwankhade3612@gmail.com>
2023-12-16 13:07:34 +0530

    test1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors")
 const dotenv = require('dotenv')
 dotenv.config()
-
+//test
 const connection = require("./db/config");
 const seatRouter = require("./routes/seats.route");
 const port = process.env.PORT || 8000;
